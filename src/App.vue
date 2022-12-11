3943ddcd2a7a2655bc83e726ece2d2ab4912d286
mas <sasasa@users.noreply.github.com>
2022-12-11 21:51:40 +0900

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <button @click="openModal">Click</button>
+
+  <Modal v-show="showModal" @modal:close="closeModal">
+    これがモーダルウィンドウです。slotの中です。
+  </Modal>
+</template>
+
+<script setup>
+import Modal from "./components/Modal.vue";
+import { ref } from "vue";
+const showModal = ref(false);
+
+const openModal = () => {
+  showModal.value = true;
+};
+
+const closeModal = () => {
+  showModal.value = false;
+};
+</script>
+
---
+++ src/components/Modal.vue
@@ -0,0 +1,39 @@
+<template>
+  <div id="overlay" @click.self="emit('modal:close')">
+    <div id="content">
+      <p><slot>これがモーダルウィンドウです。</slot></p>
+      <button @click="emit('modal:close')">Close</button>
+    </div>
+  </div>
+</template>
+<script setup>
+import { defineEmits } from "vue";
+const emit = defineEmits(["modal:close"]);
+</script>
+
+<style>
+#overlay {
+  /*　要素を重ねた時の順番　*/
+  z-index: 1;
+
+  /*　画面全体を覆う設定　*/
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.5);
+
+  /*　画面の中央に要素を表示させる設定　*/
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+#content {
+  z-index: 2;
+  width: 50%;
+  padding: 1em;
+  background: #fff;
+}
+</style>
